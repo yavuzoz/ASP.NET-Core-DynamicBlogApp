7133910ec56ee3e3f6e5807e79e416cbcfad5694
Yavuz Özbay <ozbayavz@gmail.com>
2024-10-09 13:53:26 +0200

    Set up models, configured DbContext, and installed necessary EF Core packages

--- DataAccessLayer/Migrations/20241009113552_mig1.cs
+++ DataAccessLayer/Migrations/20241009113552_mig1.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace DataAccessLayer.Migrations
 {
+    // 
     public partial class mig1 : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
