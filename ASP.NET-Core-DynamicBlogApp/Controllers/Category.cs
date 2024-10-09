b8ab2a34ace510c106f770c648bf9d2b5f07d240
Yavuz Özbay <ozbayavz@gmail.com>
2024-10-09 22:49:25 +0200

    Category backend is complete, can be tested with Category controller in the frontend

---
+++ ASP.NET-Core-DynamicBlogApp/Controllers/Category.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NET_Core_DynamicBlogApp.Controllers
+{
+    public class Category : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
